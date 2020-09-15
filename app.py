dc24e082d30e24c3fa16ce049bd884584d58aafb
young2e <pnp3737@gmail.com>
2020-09-16 01:36:36 +0900

    0916

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def login_form():
+
+    return render_template('login.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+
+    if request.method == 'POST':
+
+        if(request.form['id'] == 'admin' and request.form['pw'] == 'admin123'):
+
+            session['logged'] = True
+
+            session['user'] = request.form['id']
+
+            # return 'Hi, ' + request.form['id']
+            return render_template('index.html')
+
+        else:
+
+            return """<script>alert("wrong!");location.href='/';</script>"""
+
+    else:
+
+        return """<script>alert("not allowd!");location.href='/';</script>"""
+
+app.secret_key = 'sample_secret'
+
+
+
+if __name__ == '__main__':
+
+    app.run()
